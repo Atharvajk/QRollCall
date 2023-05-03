534f419c3f0fc9fb35b2d33ea685f994cc8a0797
Yadhnesh1234 <gangurdeyadhnesh28@gmail.com>
2023-05-03 08:00:26 +0530

    Mark Attendance feature added

--- src/controller/lecture.js
+++ src/controller/lecture.js
@@ -4,7 +4,7 @@ const { getClassStudents } = require("./classroom");
 
 //When teacher starts the attendance QR first init session
 const initAttendance = async (req, res, next) => {
-  const { facultyId, subjectId, classroom, studentId } = req.body;
+  const { facultyId, subjectId, classroom} = req.body;
   //  console.log(classroom)
   const classroomStudents = await student.find({ classroom }); // get all students of particular classroom
   //  console.log("All students from class", allStuds)
@@ -24,22 +24,19 @@ const markStudentAttendace = async (req, res, next) => {
   const { studentId, lectureId } = req.body;
   try {
     const lecture = await Lecture.findById(lectureId);
-    // Find the student subdocument with the given studentId
-    const student = lecture.allStudents.find(
-      (s) => s.studentId.toString() === studentId
-    );
-    // Toggle the isPresent field using the $ne operator
-    // const updatedLecture = await Lecture.findOneAndUpdate(
-    //   { _id: lectureId, "allStudents.studentId": studentId },
-    //   { $set: { "allStudents.$.isPresent": { $ne: student.isPresent } } },
-    //   { new: true }
-    // );
-    const updatedLecture = await Lecture.findById(lectureId)
-    const data=updatedLecture.allStudents.filter((val)=>{
+    const student=lecture.allStudents.filter((val)=>{
         return val.studentId==studentId
     })
-    console.log(data)
-    res.json(updatedLecture);
+    var markStudentAsPresent;
+     markStudentAsPresent=await Lecture.findOneAndUpdate(
+      { _id: lectureId, 'allStudents._id': student[0]._id },
+      {
+        $set: {
+          "allStudents.$.isPresent": "true",
+        }
+      },
+     );
+    res.json({message:"Student Successfully mark as present"});
   } catch (err) {
     console.error(err);
     return res.status(500).json({ message: "Internal server error" });
